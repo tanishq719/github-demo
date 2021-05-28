9ddd44460313bf957d938872f8dc1ae7aae4202b
tanishq719 <tanishqshrivastava01@gmail.com>
2021-05-28 14:38:04 +0530

    math added

---
+++ math.py
@@ -0,0 +1,22 @@
+def add(x,y):
+	return x+y
+
+def subtract(x,y):
+	temp = y - x
+	return temp
+
+def multiply(x,y):
+	return x*y
+	
+def divide(x,y):
+	temp = x/y
+	return temp
+
+def modulo(x,y):
+	return x%y
+
+def squareRoot(x):
+	return x**(1/2)
+	
+def square(x):
+	pass
